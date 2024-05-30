3b65176a71bb0f3c9b566e41fb84ac7e9bf22a52
yaseeeeeeeeeeen <muhammardyaseen@gmail.com>
2024-05-30 11:52:16 +0530

    basic_arithamatical_operators

---
+++ maths_in_python.py
@@ -0,0 +1,22 @@
+friends = int(input("Enter a how much friends you have ? :"))
+
+#add one
+# friends=friends+1
+friends += 1
+print(f"added one friend also now total is {friends}")
+
+# Subtract one
+# friends=friends-1
+friends -= 1
+print(f"Substracted one {friends}")
+
+#multiplication
+#friends=friends*2
+friends *= 2
+print(f"Multiple in 2 times now its {friends}")
+
+#Divided
+# friends=friends/2
+friends /= 2
+print(f"Divided 2 now its {friends}")
+
